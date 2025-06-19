25f6511be34bc4948c06e39eeb71af4bc6f7711c
Pratik00531 <patelpratik8489@gmail.com>
2025-06-19 10:31:40 +0530

    Final Done

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,20 @@
 import os
 import streamlit as st
 
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
-from langchain_huggingface import HuggingFaceEndpoint
 
 
 
 DB_FAISS_PATH = "vectorstore/db_faiss"
 @st.cache_resource
 def get_vectorstore():
-    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        # Remove huggingfacehub_api_token here!
+    )
     db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
     return db
 
