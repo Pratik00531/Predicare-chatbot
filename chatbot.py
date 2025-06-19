61dbf65113e294a7996024cb7aab64bf8da48a54
Pratik00531 <patelpratik8489@gmail.com>
2025-06-19 09:28:16 +0530

    Health CHatbot

---
+++ chatbot.py
@@ -0,0 +1,86 @@
+import os
+import streamlit as st
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEndpoint
+
+
+
+DB_FAISS_PATH = "vectorstore/db_faiss"
+@st.cache_resource
+def get_vectorstore():
+    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+    return db
+
+
+def set_custom_prompt(custom_prompt_template):
+    return PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+
+
+def load_llm(repo_id, hf_token):
+    return HuggingFaceEndpoint(
+        repo_id=repo_id,
+        temperature=0.5,
+        huggingfacehub_api_token=hf_token
+    )
+
+def main():
+    st.title("Ask Chatbot !")
+
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        st.chat_message(message['role']).markdown(message['content'])
+
+    prompt = st.chat_input("Enter your question here:")
+
+    if prompt:
+        st.chat_message('user').markdown(prompt)
+        st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+        CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+Dont provide anything out of the given context
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+        HUGGINGFACE_REPO_ID = "HuggingFaceH4/zephyr-7b-beta"
+        HF_TOKEN = os.environ.get("HF_TOKEN")
+        
+        try:
+            vectorstore = get_vectorstore()
+            if vectorstore is None:
+                st.error("Failed to load the vector store.")
+                return
+
+            qa_chain = RetrievalQA.from_chain_type(
+                llm=load_llm(HUGGINGFACE_REPO_ID, HF_TOKEN),
+                chain_type="stuff",
+                retriever=vectorstore.as_retriever(search_kwargs={'k': 3}),
+                return_source_documents=True,
+                chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+            )
+
+            response = qa_chain.invoke({'query': prompt})
+            result = response["result"]
+
+            st.chat_message('assistant').markdown(result)
+            st.session_state.messages.append({'role': 'assistant', 'content': result})
+
+        except Exception as e:
+            st.error(f"Error occurred: {e}")
+        
+
+if __name__ == "__main__":
+    main()
+
+
